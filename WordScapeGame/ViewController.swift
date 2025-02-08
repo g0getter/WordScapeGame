53136019c6f3d3d22211991f50ad5ddaa3cd3476
나경 <nagyeong.yeo1023@gmail.com>
2025-02-08 14:50:37 +0900

    chore: initial commit

---
+++ WordScapeGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WordScapeGame
+//
+//  Created by 여나경 on 2/8/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
